ec69c945b3a7c52109ac455289c6cbd65cb3c123
PabloGMarrero <pablo.g.marrero@gmail.com>
2019-04-21 23:34:41 -0300

    added fetch on method componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,111 +3,134 @@ import logo from "./logo.svg";
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clima: {},
+      isLoaded : false
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://api.apixu.com/v1/forecast.json?key=${
+      process.env.REACT_APP_API_KEY
+      }&q=Buenos+Aires&days=6`)
+      .then(response => response.json())
+      .then(jsonData => {
+        this.setState({ clima: jsonData, isLoaded: true});
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <div className="top">
-            <img src={logo} alt="Clima principal" className="image" />
-            <p className="temp">25 °C</p>
-            <h2 className="city">Buenos Aires, CABA, Argentina</h2>
-            <div className="feelslike">ST: 25.4 °C, H: 81 %</div>
+  const { location, current, forecast } = this.state.clima;
+
+  return this.state.isLoaded ? (
+    <div className="App">
+      <div className="container">
+        <div className="top">
+          <img src={logo} alt="Clima principal" className="image" />
+          <p className="temp">25 °C</p>
+          <h2 className="city">Buenos Aires, CABA, Argentina</h2>
+          <div className="feelslike">ST: 25.4 °C, H: 81 %</div>
+        </div>
+        <div className="bottom">
+          <div className="card">
+            <h5>Lunes</h5>
+            <h6>2018-09-26</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
+                </p>
+              <p>
+                <span className="tempmin" />
+                15
+                </p>
+            </div>
           </div>
-          <div className="bottom">
-            <div className="card">
-              <h5>Lunes</h5>
-              <h6>2018-09-26</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
+          <div className="card">
+            <h5>Martes</h5>
+            <h6>2018-09-27</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
                 </p>
-                <p>
-                  <span className="tempmin" />
-                  15
+              <p>
+                <span className="tempmin" />
+                15
                 </p>
-              </div>
             </div>
-            <div className="card">
-              <h5>Martes</h5>
-              <h6>2018-09-27</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
+          </div>
+          <div className="card">
+            <h5>Miércoles</h5>
+            <h6>2018-09-28</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
                 </p>
-                <p>
-                  <span className="tempmin" />
-                  15
+              <p>
+                <span className="tempmin" />
+                15
                 </p>
-              </div>
             </div>
-            <div className="card">
-              <h5>Miércoles</h5>
-              <h6>2018-09-28</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
+          </div>
+          <div className="card">
+            <h5>Jueves</h5>
+            <h6>2018-09-29</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
                 </p>
-                <p>
-                  <span className="tempmin" />
-                  15
+              <p>
+                <span className="tempmin" />
+                15
                 </p>
-              </div>
             </div>
-            <div className="card">
-              <h5>Jueves</h5>
-              <h6>2018-09-29</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
+          </div>
+          <div className="card">
+            <h5>Viernes</h5>
+            <h6>2018-09-30</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
                 </p>
-                <p>
-                  <span className="tempmin" />
-                  15
+              <p>
+                <span className="tempmin" />
+                15
                 </p>
-              </div>
             </div>
-            <div className="card">
-              <h5>Viernes</h5>
-              <h6>2018-09-30</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
+          </div>
+          <div className="card">
+            <h5>Sábado</h5>
+            <h6>2018-10-01</h6>
+            <img src={logo} alt="Clima diario" className="imagebottom" />
+            <div className="minmax">
+              <p>
+                <span className="tempmax" />
+                26
                 </p>
-                <p>
-                  <span className="tempmin" />
-                  15
+              <p>
+                <span className="tempmin" />
+                15
                 </p>
-              </div>
-            </div>
-            <div className="card">
-              <h5>Sábado</h5>
-              <h6>2018-10-01</h6>
-              <img src={logo} alt="Clima diario" className="imagebottom" />
-              <div className="minmax">
-                <p>
-                  <span className="tempmax" />
-                  26
-                </p>
-                <p>
-                  <span className="tempmin" />
-                  15
-                </p>
-              </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  ):(
+    <div>Cargando...</div>
+  )
   }
 }
 
